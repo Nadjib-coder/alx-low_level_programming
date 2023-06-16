3a54bf6ef386964eb15b15cbcf8653f57cae461f
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-06-16 20:54:07 +0100

    update task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return: Programming is like building a multilingual puzzle
+ * Return: "Programming is like building a multilingual puzzle
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
