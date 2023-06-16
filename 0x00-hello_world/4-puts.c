782ca0bc2e3923412294f3b17eb3f996472133a6
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-06-16 21:05:20 +0100

    update task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main: A program that print a line using puts function
+ * main - A program that print a line using puts function
  * Return: 0 (Success)
  */
 int main(void)
