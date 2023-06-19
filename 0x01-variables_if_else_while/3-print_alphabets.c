a7da18abfe1f742de3dd00c20eacaffe4ab8375d
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-06-20 00:18:11 +0100

    update task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main - Entry point
- * prints the alphabet in lowercase then uppercase
- * Return: 0 Always
+ * main - A program prints the alphabet in lowercase, and then is uppercase
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	char alphabet1[27] = "abcdefghijklmnopqrstuvwxyz";
-	char alphabet2[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i = 0;
+	char ch = 'a';
+	char CH = 'A';
 
-	while (alphabet1[i] != '\0')
+	while (ch <= 'z')
 	{
-		putchar(alphabet1[i]);
-		i++;
+		putchar(ch);
+		ch++;
 	}
-	i = 0;
-	while (alphabet2[i] != '\0')
+	while (CH <= 'Z')
 	{
-		putchar(alphabet2[i]);
-		i++;
+		putchar(CH);
+		CH++;
 	}
 	putchar('\n');
-return (0);
+	return (0);
 }
+
