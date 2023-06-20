61045ef7f4151e40ec0d8dc1fbb7a42512d6ccc1
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-06-20 22:12:15 +0100

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,19 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - print the alphabet in lowercase
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
 }
 
