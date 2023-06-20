7db97ccc12837f98f71193d4c2997114f364d99e
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-06-20 19:10:49 +0100

    update task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,30 +1,18 @@
-#include <unistd.h>
+#include "main.h"
+
 /**
- * _putchar - Writes a character to the standard output
- * @c: The character to be written
- *
- * Return: On success, returns the number of bytes written.
- * On error, returns -1 and sets errno appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-/**
- * main - Entry point of the program
- *
- * Return: Always 0 (indicating success)
+ * main - a program that write _putchar
+ * Return: Always 0
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char txt[] = "_putchar";
+	int i;
+
+	for (i = 0; txt[i] != '\0'; i++)
+	{
+		_putchar(txt[i]);
+	}
 	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,7 @@
-#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * _putchar - prototype of _putchar func
- * @c: character returned
- * Return: 1 (Success)
- */
 int _putchar(char c);
+
+#endif /* MAIN_H */
 
