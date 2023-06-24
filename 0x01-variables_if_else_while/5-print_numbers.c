65795083764a3ee45d024015411a066f24c35a4d
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-06-24 20:54:11 +0100

    update task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main - Entry point
- * prints 0123456789
- * Return: 0 always
+ * main - A program that prints all single digit numbers
+ * of base 10 starting from 0
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	printf("0123456789\n");
-return (0);
+	int i;
+
+	for (i  = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
 }
+
