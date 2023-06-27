ae10455a16406a9ac9d979fa6966ab4f5f3cf64e
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-06-27 16:48:43 +0100

    update task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,38 +8,30 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-	while (*s == ' ')
+	while (s[c])
 	{
-		s++;
-	}
-	if (*s == '-' || *s == '+')
-	{
-		if (*s == '-')
+		if (s[c] == 45)
 		{
-			sign = -1;
+			min *= -1;
 		}
-		s++;
-	}
-	while (*s >= '0' && *s <= '9')
-	{
-		if (result > (INT_MAX / 10) ||
-				(result == (INT_MAX / 10) && (*s - '0') > (INT_MAX % 10)))
+		while (s[c] >= 48 && s[c] <= 57)
 		{
-			if (sign == 1)
-			{
-				return (INT_MAX);
-			}
-			else
-			{
-				return (INT_MIN);
-			}
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
 		}
-		result = (result * 10) + (*s - '0');
-		s++;
+		if (isi == 1)
+		{
+			break;
+		}
+		c++;
 	}
-	return (sign * result);
+	ni *= min;
+	return (ni);
 }
 
