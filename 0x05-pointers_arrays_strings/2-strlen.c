b71addeea10d107ec7dbea1b0c9c8b489e0c3f83
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-06-27 01:02:51 +0100

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,11 +9,12 @@
 int _strlen(char *s)
 {
 	int i;
+	int *p;
 	int counter = 0;
 
-	for (i = 0; i < '\0'; i++)
+	p = s;
+	for (i = 0; *(p + i) < '\0'; i++)
 	{
-		*(s + i);
 		counter++;
 	}
 	return (counter);
