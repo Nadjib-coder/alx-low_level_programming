7793d46b6af0f3a19b3ee1c1bd59860314cfc057
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-06-30 01:11:59 +0100

    update task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,9 @@ char *cap_string(char *str)
 	while (str[index])
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
+		{
 			index++;
+		}
 
 		if (str[index - 1] == ' ' ||
 		    str[index - 1] == '\t' ||
@@ -29,11 +31,11 @@ char *cap_string(char *str)
 		    str[index - 1] == '{' ||
 		    str[index - 1] == '}' ||
 		    index == 0)
+		{
 			str[index] -= 32;
-
+		}
 		index++;
 	}
-
 	return (str);
 }
 
