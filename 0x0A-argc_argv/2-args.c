20185aa0119b45c2832fadbbfa684f7dea584f90
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-07-08 02:21:04 +0100

    task 3

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints all arguments it receives.
+ * @argc: number of command line arguments
+ * @argv: an array contain command ine arguments
+ * Return: 0(success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
+
