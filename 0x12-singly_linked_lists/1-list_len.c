4f1cfd61fe59cf302da9892ccf98821ccc677cd5
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-07-28 01:45:06 +0100

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * list_len -  a function that calculate the number
+ * of elements in a linked list_t list.
+ * @h: a pointer to the linked list_t list.
+ * Return: the number of elements in a linked list_t list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	if (h == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		const list_t *ptr;
+		ptr = h;
+		while (ptr != NULL)
+		{
+			count++;
+			ptr = ptr->next;
+		}
+	}
+	return (count);
+}
