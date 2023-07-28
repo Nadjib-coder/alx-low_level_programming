80d4b30fe15604ddc7ad78ac5b62891e2c56dd9c
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-07-28 03:22:29 +0100

    task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,10 @@
-section .data
-    hello_message db 'Hello, Holberton,', 0xA ; Null-terminated string with newline character (0xA)
-
-section .text
-    global _start
-
-_start:
-    ; syscall number for sys_write (1), sys_exit (60)
-    mov rax, 1                   ; syscall number for sys_write
-    mov rdi, 1                   ; file descriptor 1 (stdout)
-    mov rsi, hello_message       ; pointer to the message to print
-    mov rdx, 18                  ; length of the message (excluding null-terminator)
-    syscall                      ; invoke the syscall to print the message
-
-    ; Exit the program
-    mov rax, 60                  ; syscall number for sys_exit
-    xor rdi, rdi                 ; exit code 0
-    syscall                      ; invoke the syscall to exit
-
+	global   prgm
+	  extern    printf
+prgm:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
