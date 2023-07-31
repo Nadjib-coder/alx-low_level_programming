94af54afc61edd82ae09712045a75ef69a12bd2e
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-07-31 14:29:48 +0100

    update header file delet stdio.h libarary

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - a function that prints all the elements of a listint_t list.
+ * @h: a pointer to thefirst node in the list.
+ * Return: the number of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+
+}
+
