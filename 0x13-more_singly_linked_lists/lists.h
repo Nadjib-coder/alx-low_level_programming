03a6f0f10a55df1bfd34d29fa18b6426f4c9f9cf
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-07-31 14:23:08 +0100

    update header file add the structure

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,6 +5,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
 /* function declaration prototype */
 int _putchar(char c);
 
