5c3d0e12489c06e12ebecb0c4d432d48ce9d030a
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-08-01 00:36:09 +0100

    update task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,8 +22,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new->next = temp;
-		temp = new;
+		new->next = *head;
+		*head = new;
 		return (new);
 	}
 	while (temp && idx >= 1)
