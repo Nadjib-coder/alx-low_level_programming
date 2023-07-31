4d1f943523f928afdddae886aa78a860b07ca50e
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-07-31 23:50:53 +0100

    update task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,11 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	listint_t *temp = head;
 
-	if (head == NULL)
-	{
-		return (NULL);
-	}
-	while (index >= 1 && temp != NULL)
+	while (temp && index >= 1)
 	{
 		temp = temp->next;
 		index--;
