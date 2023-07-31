eed3a9a38db50e94cecfa12b2ff7c0d96a01b168
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-08-01 00:50:02 +0100

    update task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	new->n = n;
 	new->next = NULL;
-
 	if (idx == 0)
 	{
 		new->next = *head;
@@ -31,10 +30,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		temp = temp->next;
 		idx--;
-	}
-	if (temp == NULL)
-	{
-		return (NULL);
 	}
 	new->next = temp->next;
 	temp->next = new;
