9e46d12da75acf04267ab92c973460f0da77407a
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-08-03 01:53:49 +0100

    update last task

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -5,13 +5,14 @@
  * a linked list.
  * @head: a pointer to the first node in a linked list
  * Return: the address of the node where the loop starts, or NULL
+ * if failed.
  */
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *slow = head;
 	listint_t *fast = head;
 
-	if (head == NULL)
+	if (!head)
 	{
 		return (NULL);
 	}
