60a8389ff8aa958db3f80eb6959a060be511977f
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-08-04 02:37:23 +0100

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,19 +8,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result;
+	unsigned int result = 0;
 
 	if (b == NULL)
 	{
-		return (NULL);
+		return (0);
 	}
 	while (*b != '\0')
 	{
 		if (*b != '0' && *b != '1')
 		{
-			return (NULL);
+			return (0);
 		}
-		result << 1;
+		result <<= 1;
 		result += (*b - '0');
 		b++;
 	}
