7a3306314d94c55fad21bf11f918122d4b782cff
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-08-08 22:18:00 +0100

    update task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,11 @@ int create_file(const char *filename, char *text_content)
 	{
 		len++;
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY| O_TRUNC);
+	if (fd == -1)
+	{
+		fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	}
 	if (fd == -1)
 	{
 		return (-1);
