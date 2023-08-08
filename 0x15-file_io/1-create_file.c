8ffdf3bb4a54f7df894a650f9905750bdf11aab9
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-08-08 14:26:28 +0100

    update task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,6 @@ int create_file(const char *filename, char *text_content)
 	}
 	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	ch = write(fp, text_content, len);
-
 	if (fp == -1 || ch == -1)
 	{
 		return (-1);
