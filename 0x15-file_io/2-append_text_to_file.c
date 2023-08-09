a4a38bd5f7fdf26a610ae27f55c8e3b040aa6632
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-08-09 22:16:06 +0100

    update task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,36 +10,36 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-        int fd, w, len;
+	int fd, w, len;
 
-        if (filename == NULL)
-        {
-                return (-1);
-        }
-        if (text_content == NULL)
-        {
-                fd = open(filename, O_WRONLY | O_APPEND);
-                if (fd == -1)
-                {
-                        return (-1);
-                }
-                return (1);
-        }
-        len = 0;
-        while (text_content[len] != '\0')
-        {
-                len++;
-        }
-        fd = open(filename, O_WRONLY | O_APPEND);
-        if (fd == -1)
-        {
-                return (-1);
-        }
-        w = write(fd, text_content, len + 1);
-        if (w == -1)
-        {
-                return (-1);
-        }
-        return (1);
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_WRONLY | O_APPEND);
+		if (fd == -1)
+		{
+			return (-1);
+		}
+		return (1);
+	}
+	len = 0;
+	while (text_content[len] != '\0')
+	{
+		len++;
+	}
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	w = write(fd, text_content, len + 1);
+	if (w == -1)
+	{
+		return (-1);
+	}
+	return (1);
 }
 
