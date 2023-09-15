e954543d539dd70b36e682b25df0617554c3875d
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-09-15 15:58:29 +0100

    update task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,6 +26,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 		new_node->next = NULL;
 		new_node->prev = NULL;
+		ptr = new_node;
+		return (new_node);
 	}
 	while (ptr != NULL)
 	{
